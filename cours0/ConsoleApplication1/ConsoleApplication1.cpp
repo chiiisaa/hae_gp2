07694464cf46c88e1e3503b380f8131a354e65e8
Jessy <yua-cecil@hotmail.fr>
2020-10-01 17:32:30 +0200

    cours

--- cours0/ConsoleApplication1/ConsoleApplication1.cpp
+++ cours0/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,11 +3,14 @@
 
 #include "pch.h"
 #include <iostream>
+#include <functional>
 #include <string>
 #include "Vec2.hpp"
 #include "Vec2T.h"
 #include "Vec3T.h"
 #include "arr.h"
+#include "DynArr.h"
+#include "Node.h"
 
 using namespace std;
 
@@ -18,6 +21,146 @@ void subFunc()
 	cout << foo->get(0) << endl;
 
 	delete foo;
+}
+
+void subFunc1()
+{
+	/*DynArr<int> foo(4);
+	cout << foo.get(0) << endl;
+	foo.get(0) = 33;
+	cout << foo.get(0) << endl;
+
+	foo.resize(13);
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << foo.get(i) << endl;
+	}
+
+	foo.set(12, 9);
+	cout << foo.get(12) << endl;*/
+
+	Node* root = new Node(10);
+	root = root->heapify(9);
+	root = root->heapify(11);
+	root = root->heapify(12);
+
+	if (!root->isOk()) throw " root is not a valid heap";
+
+	root = Node::merge(root, new Node(12));
+	root = Node::merge(root, new Node(12));
+
+	Node* n = new Node(1, new Node(2), new Node(3));
+	n = n->deleteMin();
+	if(!n->isOk()) throw " root is not a valid heap";
+
+	//root = root->deleteMin();
+	root = root->remove(12);
+	if (!root->isOk()) throw " root is not a valid heap";
+
+	auto stop = 6;
+
+}
+
+int StringLengh(const char * str) {
+	int compt = 0;
+	while (*str != 0)
+	{
+		compt++;
+		str++;
+	}
+	cout << compt << endl;
+	return compt;
+}
+
+void StringCopy(char* dst, const char* src)
+{
+	while (*src) //while value(src) is not zero
+	{
+		/*char dstchar = *dst;
+		*dst = dstchar;*/
+		*dst = *src;//copy value(src) in value (dst
+
+		dst++;
+		src++;
+	}
+}
+
+void subFunc2()
+{
+	char tot2[5] = "toto";
+	auto k = 0;
+	int nb = StringLengh(tot2);
+
+	char sapin[128] = {};
+	StringCopy(sapin, "lapin");
+	cout << sapin << endl;
+}
+
+DynArr<float> * doHeapSort(float* tab, int tabSize)
+{
+	DynArr<float> * f = new DynArr<float>(tabSize);
+	Node * root = new Node(tab[0]);
+	for (int i = 1; i < tabSize; ++i)
+	{
+		root = root->heapify(tab[i]);
+	}
+	int idx = 0;
+	while (root)
+	{
+		float val = root->getMin();
+		f->set(idx, val);
+		root = root->deleteMin();
+		idx++;
+	}
+	return f;
+	/*DynArr<float> * f = new DynArr<float>(tabSize);
+	Node * root = Node::fromTabRec(tab, tabSize);
+	int idx = 0;
+	while (root)
+	{
+		float val = root->getMin();
+		f->set(idx, val);
+		root = root->deleteMin();
+		idx++;
+	}*/
+
+	//return f;
+}
+
+void heapSort() {
+
+	float tf[4] = { 1.0, 15.0, -1.0, 25.0 };
+	DynArr<float> * tab = doHeapSort(tf, 4);
+
+	for (int i = 0; i < 4; i++)
+	{
+		cout << tab->get(i) << " ";
+	}
+	cout << endl;
+}
+
+void subFunction3() {
+	DynArr<float>tab(0);
+	for (int i = 0; i < 10; i++)
+	{
+		tab.set(i, i);
+	}
+	tab.Reverse();
+	tab.push_back(16);
+
+	DynArr<float>tab0(0);
+	tab0.push_back(66);
+	tab.append(tab0);
+
+	std::function<void(float)> affiche = [](float elem)
+	{
+		printf("%f ", elem);
+	};
+	tab.iter(affiche);
+	printf("\n");
+
+	auto k = 0;
 }
 
 int main()
@@ -65,7 +208,16 @@ int main()
 
 	cout << endl;*/
 
-	subFunc();
+	//subFunc();
+
+	//subFunc1();
+
+	//subFunc2();
+
+	//heapSort();
+
+	subFunction3();
+
 
 	cout << endl;
 }
